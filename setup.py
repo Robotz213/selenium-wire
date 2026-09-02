c50509384e4fc4b5a507aa97699c464d5b194cad
Robotz213 <nicholasazs213@gmail.com>
2026-09-02 17:48:24 -0400

    att

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "selenium>=4.0.0",
         "wsproto>=0.14",
         "zstandard>=0.14.1",
+        "mitmproxy-rs>=0.12.11",
     ],
     extras_require={
         ':sys_platform == "win32"': [
